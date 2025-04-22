25f357faadbd64f99e921b39ccf25c265447e89c
luckyAce2010 <isidroacecis@gmail.com>
2025-04-22 23:03:43 +0800

    [description] Project Initialization [feature] Initialization [module] All

---
+++ src/main/java/com/example/TechNest/TechNestBackOfficeApplication.java
@@ -0,0 +1,13 @@
+package com.example.TechNest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechNestBackOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechNestBackOfficeApplication.class, args);
+	}
+
+}
